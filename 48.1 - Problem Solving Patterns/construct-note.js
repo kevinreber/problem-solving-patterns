bf8a4745b87caea881beb093f80bf475ac3b46b0
kevinreber <kevinreber1@gmail.com>
2020-10-09 19:28:50 -0700

    construct note

--- 48.1 - Problem Solving Patterns/construct-note.js
+++ 48.1 - Problem Solving Patterns/construct-note.js
@@ -1,3 +1,26 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, letters) {
+	// get frequency of chars in message and letter
+	const messageFreq = {};
+	const lettersFreq = {};
+
+	// store count of char frequency in messages
+	for (let char of message) {
+		messageFreq[char] = messageFreq[char] += 1 || 1;
+	}
+
+	// store count of char frequency in letters
+	for (let char of letters) {
+		lettersFreq[char] = lettersFreq[char] += 1 || 1;
+	}
+
+	// compare count of letterFreq char in messageFreq
+	for (let char in messageFreq) {
+		// if letter does not exist
+		if (!lettersFreq[char]) return false;
+
+		// if frequency of char in messageFreq is greater than char in lettersFreq
+		if (messageFreq[char] > lettersFreq[char]) return false;
+	}
+	return true;
 }
