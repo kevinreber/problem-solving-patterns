fb84931ecc5b3c94bd8896811fdb0d89255b0149
kevinreber <kevinreber1@gmail.com>
2020-10-09 20:51:07 -0700

    two array object

--- 48.1 - Problem Solving Patterns/two-array-object.js
+++ 48.1 - Problem Solving Patterns/two-array-object.js
@@ -10,7 +10,20 @@
  * twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3]) // {'a': 1, 'b': 2, 'c': 3, 'd': null}
  * twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4]) // {'a': 1, 'b': 2, 'c': 3}
  * twoArrayObject(['x', 'y', 'z'], [1, 2]) // {'x': 1, 'y': 2, 'z': null}
+ *
+ *
+ *  Convert the keys array into an object using Array.prototype.reduce.
+ *  In the reduce callback function, set the current value as a key in
+ *  the accumulator object, with the value set to whatever element is
+ *  at the current index in the values array.
+ *
+ *  MDN reduce -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
  */
 
 // add whatever parameters you deem necessary
-function twoArrayObject() {}
+function twoArrayObject(keys, values) {
+	return keys.reduce((obj, curr, idx) => {
+		obj[curr] = idx < values.length ? values[idx] : null;
+		return obj;
+	}, {});
+}
