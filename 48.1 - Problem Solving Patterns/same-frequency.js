7be18fa63fefce5363839d80c6b26aa0c5c19520
kevinreber <kevinreber1@gmail.com>
2020-10-09 21:00:42 -0700

    same frequency

--- 48.1 - Problem Solving Patterns/same-frequency.js
+++ 48.1 - Problem Solving Patterns/same-frequency.js
@@ -14,4 +14,28 @@
  */
 
 // add whatever parameters you deem necessary
-function sameFrequency() {}
+function sameFrequency(num1, num2) {
+	// turns numbers to string so we can check frequency
+	const str1 = num1.toString();
+	const str2 = num2.toString();
+
+	if (str1.length !== str2.length) return false;
+
+	let count1 = {};
+	let count2 = {};
+
+	// count frequency of char
+	for (let char of str1) {
+		count1[char] = (count1[char] || 0) + 1;
+	}
+
+	for (let char of str2) {
+		count2[char] = (count2[char] || 0) + 1;
+	}
+
+	// compare frequencies
+	for (let key in count1) {
+		if (count1[key] !== count2[key]) return false;
+	}
+	return true;
+}
